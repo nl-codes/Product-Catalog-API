1622a198d34f7f1748d5e476847aaf5935e1caa7
narayanLohani <narayanlohani07@gmail.com>
2025-06-23 20:37:16 +0545

    Chore: Add product, category Schema and Models

---
+++ backend/models.js
@@ -0,0 +1,26 @@
+import mongoose, { model, Schema } from "mongoose";
+
+// Category Schema
+const categorySchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        description: { type: String, default: "" },
+    },
+    { timestamps: true }
+);
+
+// Product Schema
+const productSchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        description: { type: String, default: "" },
+        price: { type: Number, required: true, min: 0 },
+        category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+        stock: { type: Number, default: 0, min: 0 },
+    },
+    { timestamps: true }
+);
+
+// Export Category and Product Models
+export const Category = model("Category", categorySchema);
+export const Product = model("Product", productSchema);
