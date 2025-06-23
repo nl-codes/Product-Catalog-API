647de6d9f49656a7462e4864221138e0bd7aacb1
narayanLohani <narayanlohani07@gmail.com>
2025-06-23 20:47:32 +0545

    Chore: Setup connection to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+import { Category, Product } from "./models.js";
 
 const app = express();
 const PORT = 3000;
@@ -8,3 +10,5 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
+const mongodb = mongoose.connect("mongodb://localhost:27017/product_catalog");
