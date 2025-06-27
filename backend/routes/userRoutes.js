e7d19e07e8b11b4fc7d36e8f9f350b956b85d9fb
narayanLohani <narayanlohani07@gmail.com>
2025-06-27 14:45:53 +0545

    Fix: Update the register route to /signup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,6 @@ import { registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/", registerUser);
+router.post("/signup", registerUser);
 
 export default router;
