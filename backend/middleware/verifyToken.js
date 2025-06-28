f7407a037a00ee895e0e32fe0b141690ae80503a
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 09:38:18 +0545

    Fix: Remove console.log

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    console.log(req.headers);
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
