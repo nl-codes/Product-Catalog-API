cea8300b296084e8b339dbea2b6eee2a4c624f39
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 09:21:36 +0545

    Fix: Remove console.log

--- backend/middleware/generateToken.js
+++ backend/middleware/generateToken.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (payload) => {
-    console.log(payload);
     const secret = process.env.JWT_SECRET_KEY;
     if (!secret) {
         throw new Error("JWT secret key is missing in environment variables");
