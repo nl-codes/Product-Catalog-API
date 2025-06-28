64196635c7edd6653e63d2dd5f7191ee2474d7bc
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 10:28:55 +0545

    Docs: generateToken from ./utils/generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,16 @@
 import jwt from "jsonwebtoken";
 
+/**
+ * @function generateToken
+ * @description Generates a JSON Web Token (JWT) using the provided payload.
+ *              The token is signed with a secret key from environment variables and expires in 1 hour.
+ *
+ * @param {Object} payload - The data to encode into the JWT
+ * @returns {string} A signed JWT token valid for 1 hour.
+ *
+ * @throws {Error} If the JWT secret key (`process.env.JWT_SECRET_KEY`) is not set.
+ *
+ */
 export const generateToken = (payload) => {
     const secret = process.env.JWT_SECRET_KEY;
     if (!secret) {
