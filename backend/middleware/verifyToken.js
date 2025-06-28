f7ecb2bf9d9bd4562d0641d3f68164a151f4367f
narayanLohani <narayanlohani07@gmail.com>
2025-06-28 09:36:57 +0545

    Feat: Middleware for verifyToken

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    console.log(req.headers);
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ error: "Token is missing" });
+    }
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, userData) => {
+        if (err) {
+            return res.status(403).json({ error: "Invalid Token" });
+        }
+        req.user = userData;
+        next();
+    });
+};
